a619e8115721ddcaa69e7b72aa2728cae8c7ecb5
Damus Bryant <damus.bryant@gmail.com>
2025-06-21 21:09:57 +1000

    Complete Phase 2: Add serializers and viewsets

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Post
+from categories.models import Category
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(Category)
