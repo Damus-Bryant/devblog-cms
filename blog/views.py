01643d230e1f7b289f265f43563c1ce68ef7fb00
Damus Bryant <damus.bryant@gmail.com>
2025-06-21 20:00:02 +1000

    Complete Phase 2: ViewSets, permissions, routing, and API structure

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Post
+from .serializers import PostSerializer
+from .permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        # Automatically assign the logged-in user as the post author
+        serializer.save(author=self.request.user)
--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Comment
+from .serializers import CommentSerializer
+from .permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        # Automatically assign the comment's author to the logged-in user
+        serializer.save(user=self.request.user)
